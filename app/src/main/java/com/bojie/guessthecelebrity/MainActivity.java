9cedb679e66665fc2c172c0fdff95c1557daa81d
Bojie Jiang <jiangbojie@gmail.com>
2015-11-16 22:54:16 -0800

    fitch the data

--- app/src/main/java/com/bojie/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/bojie/guessthecelebrity/MainActivity.java
@@ -3,7 +3,6 @@ package com.bojie.guessthecelebrity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,6 +12,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -63,7 +64,22 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             result = task.execute("http://www.posh24.com/celebrities").get();
-            Log.i("Contents of URL", result);
+            String[] splitResult = result.split("<div class=\"sidebarContainer\">");
+            Pattern p = Pattern.compile("src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
+
+            while (m.find()) {
+                System.out.println(m.group(1));
+            }
+
+             p = Pattern.compile("alt=\"(.*?)\"");
+             m = p.matcher(splitResult[0]);
+
+            while (m.find()) {
+                System.out.println(m.group(1));
+            }
+
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
